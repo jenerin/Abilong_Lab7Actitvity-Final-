48170cd65f428e0c657e12a7ddc34967ea36417f
Jenelyn Abilong <jennelynabilong22@gmail.com>
2026-05-23 06:37:14 -0700

    Update database.js

--- api/config/database.js
+++ api/config/database.js
@@ -8,7 +8,13 @@ const db = new Sequelize(
         host: process.env.DB_HOST,
         port: process.env.DB_PORT || 15705,
         dialect: 'mysql',
-        logging: false
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
 );
 
