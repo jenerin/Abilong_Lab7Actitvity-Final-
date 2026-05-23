a225c69920437b0ee26fcb447720c36e288781b9
Jenelyn Abilong <jennelynabilong22@gmail.com>
2026-05-23 06:36:41 -0700

    Update database.js

--- api/config/database.js
+++ api/config/database.js
@@ -1,15 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME || 'ipt2026_db',
-    process.env.DB_USER || 'root',
-    process.env.DB_PASSWORD || '',
+const db = new Sequelize(
+    process.env.DB_NAME || 'defaultdb',
+    process.env.DB_USER || 'avnadmin',
+    process.env.DB_PASSWORD,
     {
-        host: process.env.DB_HOST || 'localhost',
-        port: process.env.DB_PORT || 3306,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 15705,
         dialect: 'mysql',
         logging: false
     }
 );
 
-module.exports = sequelize;
+module.exports = db;
