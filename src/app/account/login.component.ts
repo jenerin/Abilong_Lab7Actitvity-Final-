b7b034abf0156e476dd6072714b4df1638ea87f3
Jenelyn Abilong <jennelynabilong22@gmail.com>
2026-05-24 01:32:21 -0700

    Update login.component.ts

--- src/app/account/login.component.ts
+++ src/app/account/login.component.ts
@@ -16,7 +16,7 @@ export class LoginComponent implements OnInit {
   loading = false;
   submitted = false;
   returnUrl: string = '';
-  showPassword = false; // Add this property
+  showPassword = false; // Add this property for password visibility
 
   constructor(
     private formBuilder: FormBuilder,
