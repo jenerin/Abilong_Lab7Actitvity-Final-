f126f126d88139da250f6e0800014c935dc4e236
Jenelyn Abilong <jennelynabilong22@gmail.com>
2026-05-24 20:38:07 -0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
 
 import { AccountService } from './_services';
 import { Account, Role } from './_models';
@@ -8,15 +8,27 @@ import { Account, Role } from './_models';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   account?: Account | null;
   readonly Role = Role;
 
-  constructor(private accountService: AccountService) {
-    this.accountService.account.subscribe((x) => (this.account = x));
+  constructor(
+    private accountService: AccountService,
+    private cdr: ChangeDetectorRef // 🚀 FIXED: Inject ChangeDetectorRef to force UI refreshes
+  ) {}
+
+  ngOnInit() {
+    // 🚀 FIXED: Subscribing within ngOnInit handles component initialization cycles much better
+    this.accountService.account.subscribe((x) => {
+      this.account = x;
+      
+      // Force Angular to look for template changes (*ngIf="!account" toggles)
+      this.cdr.detectChanges();
+    });
   }
 
   logout() {
     this.accountService.logout();
+    this.cdr.detectChanges();
   }
 }
