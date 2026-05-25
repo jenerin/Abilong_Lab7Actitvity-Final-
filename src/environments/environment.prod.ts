fbf1bb0a68210f9c5c3d1e9c31bb9d9ca0da8f03
Jenelyn Abilong <jennelynabilong22@gmail.com>
2026-05-24 21:04:05 -0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://abilong-lab7actitvity-final-backend.onrender.com'
+  apiUrl: 'https://abilong-lab7actitvity-final-backend.onrender.com',
+  useFakeBackend: false
 };
